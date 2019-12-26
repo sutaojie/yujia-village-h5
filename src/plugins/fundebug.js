ef184e44091c23e00e85ba78992918beb43af688
苏赵勇 <sutaojie@gmail.com>
2019-12-26 16:21:59 +0800

    fundebug

--- src/plugins/fundebug.js
+++ src/plugins/fundebug.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import * as fundebug from "fundebug-javascript";
 import fundebugVue from "fundebug-vue";
-fundebug.init({
-    apikey: "6bd9c0ab954a13c99bc1998fa8044a91d5c00ca54dada835f15702aab21f3f71"
-})
 
 // eslint-disable-next-line no-undef
 if (process.env.NODE_ENV !== 'production') {
+  fundebug.init({
+      apikey: "6bd9c0ab954a13c99bc1998fa8044a91d5c00ca54dada835f15702aab21f3f71"
+  })
   fundebugVue(fundebug, Vue);
 }
